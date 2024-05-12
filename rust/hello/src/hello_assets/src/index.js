8a60944832b1dff202656d2c557767cdea2e7ec1
William Wang <bn.william.w@gmail.com>
2024-05-11 21:11:37 -0400

    different images

--- rust/hello/src/hello_assets/src/index.js
+++ rust/hello/src/hello_assets/src/index.js
@@ -23,3 +23,26 @@ document.querySelector("form").addEventListener("submit", async (e) => {
 
   return false;
 });
+
+
+function populateImages() {
+  var imageUrls = [
+    "https://d1jyxxz9imt9yb.cloudfront.net/medialib/3803/image/p1300x1300/leopard_2.webp",
+    "https://d1jyxxz9imt9yb.cloudfront.net/medialib/3848/image/p1300x1300/AdobeStock_384667533_431571.webp",
+    "https://d1jyxxz9imt9yb.cloudfront.net/medialib/3980/image/p1300x1300/Mignon-Gorilla5_reduced.webp",
+    // Add more image URLs here
+  ];
+
+  // Get the placeholder element where the images will be rendered
+  var placeholder = document.getElementById("placeholder");
+
+  // Loop through the image URLs and create <img> elements
+  for (var i = 0; i < imageUrls.length; i++) {
+    var imageUrl = imageUrls[i];
+    var img = document.createElement("img");
+    img.src = imageUrl;
+    placeholder.appendChild(img);
+  }
+}
+
+populateImages();
